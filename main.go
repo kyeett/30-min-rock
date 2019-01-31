1cdaaeffd521912e57e5a1326329017ea4977f5d
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2019-01-31 21:41:00 +0100

    starting file

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/peterhellberg/gfx"
+)
+
+const (
+	screenWidth  = 400
+	screenHeight = 400
+	coinSize     = 32
+)
+
+type Player struct {
+	gfx.Rect
+}
+
+type Coin struct {
+	gfx.Rect
+}
+
+type Game struct {
+}
+
+func main() {
+
+}
